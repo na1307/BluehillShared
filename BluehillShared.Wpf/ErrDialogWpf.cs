3be4151fa8bf87e5e13c5d483d17ba635effbdb5
na1307 <na1307@outlook.kr>
2022-10-27 15:08:16 +0900

    Wpf

--- BluehillShared.Wpf/ErrDialogWpf.cs
+++ BluehillShared.Wpf/ErrDialogWpf.cs
@@ -1,11 +1,13 @@
-﻿namespace BluehillShared;
+﻿using System.Windows;
+
+namespace BluehillShared;
 
 public static class ErrDialogWpf {
     public static void ErrMsg(string message, bool exit) {
         ErrMsg(message);
-        if (exit) Application.Current.Shutdown(1);
+        if (exit) System.Windows.Application.Current.Shutdown(1);
     }
 
     public static MessageBoxResult ErrMsg(string message) => ErrMsg(message, MessageBoxButton.OK);
-    public static MessageBoxResult ErrMsg(string message, MessageBoxButton button) => MessageBox.Show(message, "오류", button, MessageBoxImage.Error);
+    public static MessageBoxResult ErrMsg(string message, MessageBoxButton button) => System.Windows.MessageBox.Show(message, "오류", button, MessageBoxImage.Error);
 }
